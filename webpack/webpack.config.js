58f3676a4747efa06976769bdef39800738c462e
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-12-30 10:36:55 -0500

    restructure based on guide

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -3,7 +3,8 @@ const CopyPlugin = require('copy-webpack-plugin');
 module.exports = {
    mode: "production",
    entry: {
-      background: path.resolve(__dirname, "..", "src", "background.ts"),
+      content: path.resolve(__dirname, "..", "src", "content.js"),
+      popup: path.resolve(__dirname, "..", "src", "popup.js"),
    },
    output: {
       path: path.join(__dirname, "../dist"),
