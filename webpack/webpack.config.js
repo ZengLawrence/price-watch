09b54e81a13095b0bc1273e5cd6c89c4213e7cf2
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-12-30 14:06:01 -0500

    convert to ts for type checking

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
    mode: "production",
    entry: {
       content: path.resolve(__dirname, "..", "src", "content.ts"),
-      popup: path.resolve(__dirname, "..", "src", "popup.js"),
+      popup: path.resolve(__dirname, "..", "src", "popup.ts"),
    },
    output: {
       path: path.join(__dirname, "../dist"),
