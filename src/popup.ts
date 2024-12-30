c59cf3dd06501234ef35e17d30e6dce9c23cd48f
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-12-30 14:26:40 -0500

    show price

--- src/popup.ts
+++ src/popup.ts
@@ -1,1 +1,7 @@
-console.log('This is a popup!');
+const productInfo = document.getElementById('price-info');
+if (productInfo) {
+    const price = 100;
+    const priceEl = document.createElement('div');
+    priceEl.textContent = `Price: ${price}`;
+    productInfo.appendChild(priceEl);
+}
