89ccac1854c9c5325bb3d2b3fe193eed5ea31d91
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-12-31 15:16:28 -0500

    Remove webpack directory (#9)
    Flatter layout for easier nagvigation.

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,12 @@ module.exports = (env, argv) => {
    return {
       mode: "production",
       entry: {
-         content: path.resolve(__dirname, "..", "src", "content.ts"),
-         popup: path.resolve(__dirname, "..", "src", "popup.ts"),
-         background: path.resolve(__dirname, "..", "src", "background.ts"),
+         content: path.resolve(__dirname, "src", "content.ts"),
+         popup: path.resolve(__dirname, "src", "popup.ts"),
+         background: path.resolve(__dirname, "src", "background.ts"),
       },
       output: {
-         path: path.join(__dirname, "../dist"),
+         path: path.join(__dirname, "dist"),
          filename: "[name].js",
       },
       resolve: {
