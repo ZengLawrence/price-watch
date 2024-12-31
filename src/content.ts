5c3e5d721c1dd35d64ddb9844f62aac51922e151
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-12-31 09:52:33 -0500

    allow multiple ways to get price

--- src/content.ts
+++ src/content.ts
@@ -1,19 +1,24 @@
-const attachAccessoryFeature = document.getElementById('attachAccessoryModal_feature_div');
+function getPriceByAttachAccessoryFeature() {
+    const attachAccessoryFeature = document.getElementById('attachAccessoryModal_feature_div');
 
-if (attachAccessoryFeature) {
-    console.log('attachAccessoryFeature found');
-    const price = document.getElementById('attach-base-product-price')?.getAttribute('value');
-    console.log('price=' + price);
-    const asin = document.getElementById('attach-baseAsin')?.getAttribute('value');
-    console.log('asin=' + asin);
-    const description = document.getElementById('productTitle')?.innerText;
-    console.log('description=' + description);
-
-    if (price) {
-        chrome.runtime.sendMessage({ type: 'price-info-update', priceInfo: { price: parseFloat(price) } });
+    let price = null;
+    if (attachAccessoryFeature) {
+        console.log('attachAccessoryFeature found');
+        price = document.getElementById('attach-base-product-price')?.getAttribute('value');
+        console.log('price=' + price);
+        const asin = document.getElementById('attach-baseAsin')?.getAttribute('value');
+        console.log('asin=' + asin);
+        const description = document.getElementById('productTitle')?.innerText;
+        console.log('description=' + description);
     } else {
-        console.error('Price is null or undefined');
+        console.log('attachAccessoryFeature not found');
     }
+    return price;
+}
+
+const price = getPriceByAttachAccessoryFeature();
+if (price) {
+    chrome.runtime.sendMessage({ type: 'price-info-update', priceInfo: { price: parseFloat(price) } });
 } else {
-    console.log('attachAccessoryFeature not found');
+    console.error('Price is null or undefined');
 }
