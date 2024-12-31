0edf8f44535bead773484b2476ab714226327ed3
Lawrence Zeng <lawrence.zeng@gmail.com>
2024-12-31 14:50:55 -0500

    extract function

--- src/content.ts
+++ src/content.ts
@@ -6,31 +6,40 @@ interface TwisterPriceData {
     }[];
 }
 
+function getPrice(twister: Element): number | null {
+    const json = twister.querySelector('.twister-plus-buying-options-price-data')?.textContent;
+    if (json) {
+        const priceData: TwisterPriceData = JSON.parse(json);
+        if (priceData) {
+            const g = priceData.desktop_buybox_group_1;
+            const price = g[g.length - 1].priceAmount;
+            console.log('price=' + price);
+        }
+    }
+    return null;
+}
+
 function getPriceInfo(): PriceInfo | null {
     const twister = document.querySelector('#twisterPlusWWDesktop');
 
     if (twister) {
-        let price = 0;
-        const json = twister.querySelector('.twister-plus-buying-options-price-data')?.textContent;
-        if (json) {
-            const priceData: TwisterPriceData = JSON.parse(json);
-            if (priceData) {
-                const g = priceData.desktop_buybox_group_1;
-                price = g[g.length - 1].priceAmount;
-                console.log('price=' + price);
-            }
+        const price = getPrice(twister);
+
+        if (!price) {
+            console.log('Price is null or undefined');
+            return null;
         }
 
         const asin = document.getElementById('twister-plus-asin')?.getAttribute('value')!;
         console.log('asin=' + asin);
         const description = document.getElementById('productTitle')?.innerText;
         console.log('description=' + description);
-        return {price, asin, description};
+        return { price, asin, description };
     }
     return null;
 }
 
-const  priceInfo = getPriceInfo();
+const priceInfo = getPriceInfo();
 if (priceInfo) {
     chrome.runtime.sendMessage({ type: 'price-info-update', priceInfo });
 } else {
