065903b290c8d45b0064dc910869727cf3fae7be
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-01-11 10:23:21 -0500

    extract to new file

---
+++ src/parsePrice.ts
@@ -0,0 +1,51 @@
+import { PriceInfo } from "./PriceInfo";
+
+interface TwisterPriceData {
+    desktop_buybox_group_1: {
+        priceAmount: number;
+        buyingOptionType: string;
+    }[];
+}
+function getPrice(twister: Element): number | null {
+    const json = twister.querySelector('.twister-plus-buying-options-price-data')?.textContent;
+    if (json) {
+        const priceData: TwisterPriceData = JSON.parse(json);
+        if (priceData) {
+            const newItemBuyOptions = getBuyOptions(priceData, p => p.buyingOptionType === 'NEW');
+            const price = head(newItemBuyOptions)?.priceAmount || null;
+            console.log('price=' + price);
+            return price;
+        }
+    }
+    return null;
+}
+function head<T>(arr: T[]): T | undefined {
+    if (arr && arr.length > 0) {
+        return arr[0];
+    } else {
+        return undefined;
+    }
+}
+function getBuyOptions(priceData: TwisterPriceData, filterFn: (buyOption: { buyingOptionType: string; }) => void) {
+    const g = priceData.desktop_buybox_group_1;
+    return g?.filter(filterFn);
+}
+export function getPriceInfo(): PriceInfo | null {
+    const twister = document.querySelector('#twisterPlusWWDesktop');
+
+    if (twister) {
+        const price = getPrice(twister);
+
+        if (!price) {
+            console.log('Price is null or undefined');
+            return null;
+        }
+
+        const asin = (document.getElementById('twister-plus-asin')?.getAttribute('value'))!;
+        console.log('asin=' + asin);
+        const description = document.getElementById('productTitle')?.innerText;
+        console.log('description=' + description);
+        return { price, asin, description };
+    }
+    return null;
+}
