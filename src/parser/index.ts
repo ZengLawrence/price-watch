31b9634e132eb6905d848dcfbf436b2720f9b48b
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-01-12 15:23:12 -0500

    initial version

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -1,6 +1,7 @@
+import _ from 'lodash';
 import { BaseProduct, Product } from "../product";
-import * as twister from './twisterPrice';
 import * as corePriceFeature from './corePriceFeature';
+import * as twister from './twisterPrice';
 
 export function getProduct(): Product | null {
     const baseProduct =  getBaseProduct(document);
@@ -24,3 +25,19 @@ function getBaseProduct(document: Document): BaseProduct | null {
     }
     return corePriceFeature.getBaseProduct(document);
 }
+
+export function getProductsInCart(document: Document): BaseProduct[] {
+    const cart = document.querySelector('div[data-cart-type="Retail_Cart"].ewc-active-cart--selected');
+    if (cart) {
+        return _.map(cart.querySelectorAll('div[data-asin]'), (el): BaseProduct | null => {
+            const asin = el.getAttribute('data-asin');
+            const price = el.getAttribute('data-price');
+            if (asin && price) {
+                return { asin, price: parseFloat(price) };
+            }
+            return null;
+        }).filter((product): product is BaseProduct => product !== null);
+    }
+    console.log('cart not found');
+    return [];
+} 
