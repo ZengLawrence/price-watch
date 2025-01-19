9f84ec5d10bd171c0cefb6eb209cc9319ed477dd
Lawrence Zeng <lawrence.zeng@gmail.com>
2025-01-19 12:08:40 -0500

    explicitly define type

--- src/buySignal.ts
+++ src/buySignal.ts
@@ -1,20 +1,24 @@
 import { ProductPrice } from "./product";
 
-export function buySignal(priceInfo: ProductPrice, existingPriceInfo: ProductPrice) {
+export interface BuySignal {
+    shouldBuy: boolean;
+    reason: string;
+    previousPrice: number;
+}
+
+export function buySignal(priceInfo: ProductPrice, existingPriceInfo: ProductPrice): BuySignal {
     const previousPrice = existingPriceInfo.price;
     if (priceInfo.price < existingPriceInfo.price) {
-        return { 
-            shouldBuy: true, 
+        return {
+            shouldBuy: true,
             reason: 'Price lowered',
             previousPrice,
         };
     } else {
-        return { 
-            shouldBuy: false, 
+        return {
+            shouldBuy: false,
             reason: 'Price not changed or higher',
             previousPrice,
         };
     }
 }
-
-export type BuySignal = ReturnType<typeof buySignal>;
